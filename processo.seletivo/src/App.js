8bca262b7a360ecfbc92198e369743f9e9cef2cb
Vinicius-Viana09 <viniciusniviana@gmail.com>
2021-10-28 11:21:13 -0300

    Estrutura

--- processo.seletivo/src/App.js
+++ processo.seletivo/src/App.js
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import { Component } from 'react';
 
 export default class App extends Component {
   render() {
@@ -10,6 +10,19 @@ export default class App extends Component {
         <input type="text" value={this.state}></input>
 
         <button>Buscar</button>
+
+        <table>
+          <thead>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Descrição</th>
+            <th>Data</th>
+            <th>Tamanho</th>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
       </div>
     )
   }
